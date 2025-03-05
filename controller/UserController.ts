333d47a54536ab0e2f4bc612bf75c734f8040a4d
chetan <chetan.sq05@gmail.com>
2025-03-05 17:33:13 +0530

    changes

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -39,11 +39,71 @@ export const getAllUsers = async (request: Request, response: Response) => {
 // @url : http://127.0.0.1:9499/users/register
 
 
+// export const registerUser = async (request: Request, response: Response) => {
+
+//     const errors = validationResult(request);
+
+//     // express validator error handling
+
+//     if (!errors.isEmpty()) {
+//         return response.status(400).json({ errors: errors.array() });
+//     }
+
+//     try {
+
+//         let { username, email, password } = request.body;
+
+//         // check for user exists
+//         const userObj = await UserTable.findOne({ email: email });
+
+//         if (userObj) {
+//             return response.status(400).json({
+//                 error: "The user already exists"
+//             });
+//         }
+
+//         // password bcryption
+
+//         const salt = await bcryptjs.genSalt(10);
+//         const hashPassword = await bcryptjs.hash(password, salt);
+
+//         // gravatar url
+
+//         const imageUrl = gravatar.url(email, {
+//             size: "200",
+//             rating: "pg",
+//             default: "mm"
+//         });
+
+//         // insert in db
+
+//         const newUser: IUser = {
+//             username: username,
+//             email: email,
+//             password: hashPassword,
+//             imageUrl: imageUrl,
+//             isAdmin: false
+//         }
+
+//         let theUser: IUser | undefined | null = await new UserTable(newUser).save();
+
+//         if (theUser) {
+//             return response.status(200).json({
+//                 data: theUser,
+//                 msg: "Your Registration Successfully! ✅"
+//             });
+//         }
+
+//     } catch (error: any) {
+//         return response.status(500).json({
+//             data: null,
+//             error: error.message
+//         });
+//     }
+// }
+
 export const registerUser = async (request: Request, response: Response) => {
-
     const errors = validationResult(request);
-
-    // express validator error handling
 
     if (!errors.isEmpty()) {
         return response.status(400).json({ errors: errors.array() });
@@ -51,15 +111,15 @@ export const registerUser = async (request: Request, response: Response) => {
 
     try {
 
+
         let { username, email, password } = request.body;
 
-        // check for user exists
+        // check if user exists
+
         const userObj = await UserTable.findOne({ email: email });
 
         if (userObj) {
-            return response.status(400).json({
-                error: "The user already exists"
-            });
+            return response.status(200).json({ error: "The user already exists" });
         }
 
         // password bcryption
@@ -69,11 +129,7 @@ export const registerUser = async (request: Request, response: Response) => {
 
         // gravatar url
 
-        const imageUrl = gravatar.url(email, {
-            size: "200",
-            rating: "pg",
-            default: "mm"
-        });
+        const imageUrl = gravatar.url(email, { size: "200", rating: "pg", default: "mm" });
 
         // insert in db
 
@@ -90,17 +146,80 @@ export const registerUser = async (request: Request, response: Response) => {
         if (theUser) {
             return response.status(200).json({
                 data: theUser,
-                msg: "Your Registration Successfully! ✅"
+                msg: "User is created...✅"
             });
         }
-
     } catch (error: any) {
-        return response.status(500).json({
-            data: null,
-            error: error.message
-        });
+        return response.status(500).json({ error: error.message })
     }
+
 }
+
+// export const loginUser = async (request: Request, response: Response) => {
+//     const errors = validationResult(request);
+
+//     if (!errors.isEmpty()) {
+//         return response.status(400).json({ errors: errors.array() });
+//     }
+
+//     try {
+//         let { email, password } = request.body;
+
+//         // check if the user exists
+
+//         const userObj = await UserTable.findOne({ email: email });
+
+//         if (!userObj) {
+//             return response.status(500).json({
+//                 data: null,
+//                 error: "invalid Email Address"
+//             });
+//         }
+
+//         // check for password
+
+//         let isMatch: boolean = await bcryptjs.compare(password, userObj.password);
+
+//         if (!isMatch) {
+//             return response.status(500).json({
+//                 data: null,
+//                 error: "Invalid Password!"
+//             });
+//         }
+
+//         // create token
+
+//         const secretKey: string | undefined = process.env.JWT_SECRET_KEY;
+
+//         const payload: any = {
+//             id: userObj.id,
+//             email: userObj.email
+//         }
+
+
+//         if (secretKey && payload) {
+//             jwt.sign(payload, secretKey, { expiresIn: 10000 }, (error, encoded) => {
+//                 if (error) {
+//                     throw error
+//                 }
+
+//                 if (encoded) {
+//                     return response.status(200).json({
+//                         data: userObj,
+//                         token: encoded,
+//                         msg: "Login success! ✅"
+//                     });
+//                 }
+//             });
+//         }
+
+
+//     } catch (error: any) {
+//         return response.status(500).json({
+//             error: error.message
+//         });
+//     }
+// }
 
 export const loginUser = async (request: Request, response: Response) => {
     const errors = validationResult(request);
@@ -110,6 +229,7 @@ export const loginUser = async (request: Request, response: Response) => {
     }
 
     try {
+
         let { email, password } = request.body;
 
         // check if the user exists
@@ -119,18 +239,18 @@ export const loginUser = async (request: Request, response: Response) => {
         if (!userObj) {
             return response.status(500).json({
                 data: null,
-                error: "invalid Email Address"
+                error: "Invalid Email Address ❌"
             });
         }
 
-        // check for password
+        // check password
 
         let isMatch: boolean = await bcryptjs.compare(password, userObj.password);
 
         if (!isMatch) {
             return response.status(500).json({
                 data: null,
-                error: "Invalid Password!"
+                error: "Invalid Password ❌"
             });
         }
 
@@ -143,23 +263,19 @@ export const loginUser = async (request: Request, response: Response) => {
             email: userObj.email
         }
 
-
         if (secretKey && payload) {
             jwt.sign(payload, secretKey, { expiresIn: 10000 }, (error, encoded) => {
-                if (error) {
-                    throw error
-                }
+                if (error) throw error
 
                 if (encoded) {
                     return response.status(200).json({
                         data: userObj,
                         token: encoded,
-                        msg: "Login success! ✅"
+                        msg: "Login Success! ✅"
                     });
                 }
             });
         }
-
 
     } catch (error: any) {
         return response.status(500).json({
